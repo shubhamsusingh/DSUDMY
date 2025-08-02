104f5380bf75576ee783da5024962874e228df8a
shubham singh <singhshubham68738@gmail.com>
2025-08-02 15:19:23 +0530

    working

--- practice/sorting/BubbleSort/BubbleSort.java
+++ practice/sorting/BubbleSort/BubbleSort.java
@@ -1,9 +1,24 @@
 package practice.sorting.BubbleSort;
 
 public class BubbleSort {
+    // first method:-
+
+    // public void bubbleSort(int[] array) {
+    // for (int i = 0; i < array.length - 1; i++) {
+    // for (int j = 0; j < array.length - i - 1; j++) {
+    // if (array[j] > array[j + 1]) {
+    // int temp = array[j];
+    // array[j] = array[j + 1];
+    // array[j + 1] = temp;
+    // }
+    // }
+    // }
+    // }
+
+    // Second Method:-
     public void bubbleSort(int[] array) {
-        for (int i = 0; i < array.length - 1; i++) {
-            for (int j = 0; j < array.length - i - 1; j++) {
+        for (int i = array.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
                 if (array[j] > array[j + 1]) {
                     int temp = array[j];
                     array[j] = array[j + 1];
