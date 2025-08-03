e074b24de81df7a6155d54de144bb84a1aea538f
shubham singh <singhshubham68738@gmail.com>
2025-08-03 17:55:52 +0530

    working

--- practice/Tree/Main.java
+++ practice/Tree/Main.java
@@ -7,7 +7,13 @@ public class Main {
         bst.insert(40);
         bst.insert(4);
         bst.insert(24);
+        bst.insert(60);
+        bst.insert(22);
+        bst.insert(2);
+        bst.insert(6);
         System.out.println(bst.contains(24));
         System.out.println("BFS : " + bst.BFS().toString());
+        System.out.println("DFS PreOrder : " + bst.DFSPreeOrder().toString());
+        System.out.println("DFSInOrder : " + bst.DFSInOrder().toString());
     }
 }
