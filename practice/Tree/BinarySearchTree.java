a1df3636b78aa009d918ad7f8e8a9a0912d8fd00
shubham singh <singhshubham68738@gmail.com>
2025-08-03 10:56:42 +0530

    working tree traversal

---
+++ practice/Tree/BinarySearchTree.java
@@ -0,0 +1,80 @@
+package practice.Tree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BinarySearchTree {
+    private Node root;
+
+    class Node {
+        int val;
+        Node left;
+        Node right;
+
+        public Node(int val) {
+            this.val = val;
+            left = null;
+            right = null;
+        }
+    }
+
+    public boolean insert(int val) {
+        Node newNode = new Node(val);
+        if (root == null) {
+            root = newNode;
+            return true;
+        }
+        Node temp = root;
+        while (true) {
+            if (temp.val == newNode.val) {
+                return false;
+            }
+            if (newNode.val < temp.val) {
+                if (temp.left == null) {
+                    temp.left = newNode;
+                    return true;
+                }
+                temp = temp.left;
+            } else {
+                if (temp.right == null) {
+                    temp.right = newNode;
+                    return true;
+                }
+                temp = temp.right;
+            }
+        }
+    }
+
+    public boolean contains(int val) {
+        Node temp = root;
+        while (temp != null) {
+            if (val < temp.val) {
+                temp = temp.left;
+            } else if (val > temp.val) {
+                temp = temp.right;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public ArrayList<Integer> BFS() {
+        Node currentNode = root;
+        Queue<Node> queue = new LinkedList<>();
+        ArrayList<Integer> result = new ArrayList<>();
+        queue.add(root);
+        while (queue.size() > 0) {
+            currentNode = queue.remove();
+            result.add(currentNode.val);
+            if (currentNode.left != null) {
+                queue.add(currentNode.left);
+            }
+            if (currentNode.right != null) {
+                queue.add(currentNode.right);
+            }
+        }
+        return result;
+    }
+}
