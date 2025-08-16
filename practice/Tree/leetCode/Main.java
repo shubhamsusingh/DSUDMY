fd91fd95939d5e603fe8f900ad3bd56fefe557f4
shubham singh <singhshubham68738@gmail.com>
2025-08-17 02:14:34 +0530

    working on tree

---
+++ practice/Tree/leetCode/Main.java
@@ -0,0 +1,20 @@
+package practice.Tree.leetCode;
+
+public class Main {
+    public static void main(String[] args) {
+        Treedemo tree = new Treedemo();
+        tree.insert(10);
+        tree.insert(9);
+        tree.insert(8);
+        tree.insert(7);
+        tree.insert(6);
+        tree.insert(5);
+        tree.insert(4);
+        tree.insert(3);
+        tree.insert(2);
+        tree.insert(1);
+        tree.insert(15);
+        tree.insert(12);
+        System.out.println(tree.BFS());
+    }
+}
