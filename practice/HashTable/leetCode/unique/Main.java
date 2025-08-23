7b4cbed4670f867acde9baff5ea7489f664a6cc9
shubham singh <singhshubham68738@gmail.com>
2025-08-23 22:58:08 +0530

    working on question

---
+++ practice/HashTable/leetCode/unique/Main.java
@@ -0,0 +1,9 @@
+package practice.HashTable.leetCode.unique;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = { 1, 1, 2, 3, 3, 4, 4 };
+        Unique u = new Unique();
+        u.unique(arr);
+    }
+}
